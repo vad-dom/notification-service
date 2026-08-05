9a5dfab939857bd96565657dc1a77901b506878f
vad-dom <vadim_dombinskii@mail.ru>
2026-08-05 20:59:50 +0300

    constructor injection в resolver

--- src/app/Services/NotificationProviderResolver.php
+++ src/app/Services/NotificationProviderResolver.php
@@ -9,13 +9,18 @@ use App\Services\NotificationProviders\EmailProviderMock;
 use App\Services\NotificationProviders\SmsProviderMock;
 use InvalidArgumentException;
 
-class NotificationProviderResolver
+readonly class NotificationProviderResolver
 {
+    public function __construct(
+        private SmsProviderMock $smsProvider,
+        private EmailProviderMock $emailProvider,
+    ) {}
+
     public function resolve(Notification $notification): NotificationProviderInterface
     {
         return match ($notification->batch->channel) {
-            NotificationChannel::Sms => app(SmsProviderMock::class),
-            NotificationChannel::Email => app(EmailProviderMock::class),
+            NotificationChannel::Sms => $this->smsProvider,
+            NotificationChannel::Email => $this->emailProvider,
             default => throw new InvalidArgumentException('Unsupported notification channel.'),
         };
     }
