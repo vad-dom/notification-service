3b460229f01e3f0ea5910365d3829f7569088bbb
vad-dom <vadim_dombinskii@mail.ru>
2026-08-05 23:16:00 +0300

    использование контактов recipient mock-провайдерами

--- src/app/Services/NotificationProviders/EmailProviderMock.php
+++ src/app/Services/NotificationProviders/EmailProviderMock.php
@@ -4,11 +4,18 @@ namespace App\Services\NotificationProviders;
 
 use App\Interfaces\NotificationProviderInterface;
 use App\Models\Notification;
+use InvalidArgumentException;
 
 class EmailProviderMock implements NotificationProviderInterface
 {
     public function send(Notification $notification): string
     {
+        $email = $notification->recipient?->email;
+
+        if ($email === null || $email === '') {
+            throw new InvalidArgumentException('Recipient email is required to send email.');
+        }
+
         return 'email-'.$notification->id;
     }
 }
--- src/app/Services/NotificationProviders/SmsProviderMock.php
+++ src/app/Services/NotificationProviders/SmsProviderMock.php
@@ -4,11 +4,18 @@ namespace App\Services\NotificationProviders;
 
 use App\Interfaces\NotificationProviderInterface;
 use App\Models\Notification;
+use InvalidArgumentException;
 
 class SmsProviderMock implements NotificationProviderInterface
 {
     public function send(Notification $notification): string
     {
+        $phone = $notification->recipient?->phone;
+
+        if ($phone === null || $phone === '') {
+            throw new InvalidArgumentException('Recipient phone is required to send SMS.');
+        }
+
         return 'sms-'.$notification->id;
     }
 }
